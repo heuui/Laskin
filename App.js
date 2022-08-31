5061344fca00dabe059751317080eb710b23cefc
heuui <henni.haavistola@gmail.com>
2022-08-31 13:09:36 +0300

    valmis projekti

--- App.js
+++ App.js
@@ -1,10 +1,48 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, StyleSheet, Text, View, TextInput } from 'react-native';
 
 export default function App() {
+
+  const [numberOne, setNumberOne] = useState(0);
+  const [numberTwo, setNumberTwo] = useState(0);
+  const [result, setResult] = useState(0);
+
+  const calculateSum = () => {
+    setResult(numberOne + numberTwo)
+  }
+
+  const calculateSubtraction = () => {
+    setResult(numberOne - numberTwo)
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Result {result}</Text>
+      <TextInput
+              placeholder={'Enter number'}
+              style={{ borderColor: 'gray', borderWidth: 1, width: 200, height: 50 }}
+              keyboardType='number-pad'
+              onChangeText={numberOne => setNumberOne(Number(numberOne))}
+              value={numberOne}
+      />
+      <TextInput
+              placeholder={'Enter number'}
+              style={{ borderColor: 'gray', borderWidth: 1, width: 200, height: 50 }}
+              keyboardType='number-pad'
+              onChangeText={numberTwo => setNumberTwo(Number(numberTwo))}
+              value={numberTwo}
+      />
+      <View style={{ flexDirection:"row" }}>
+        <Button 
+          onPress={calculateSum}
+          title="+"
+        />
+        <Button 
+          onPress={calculateSubtraction} 
+          title="-"
+        />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
